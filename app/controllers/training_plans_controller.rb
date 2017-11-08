c52037abee2e064c033bee7d215c71bf00d875e1
dejanacim <dejanacim@gmail.com>
2017-11-08 14:34:04 +0100

    Add training plan show page

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -3,4 +3,8 @@ class TrainingPlansController < ApplicationController
   def index
     @training_plans = TrainingPlan.all
   end
+
+  def show
+    @training_plan = TrainingPlan.find(params[:id])
+  end
 end
