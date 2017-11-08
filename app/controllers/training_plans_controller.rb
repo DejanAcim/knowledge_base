f3944f0031a87367a4695c79f4c3139d81129df3
dejanacim <dejanacim@gmail.com>
2017-11-08 20:21:53 +0100

    Create new Training plan form

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -7,4 +7,26 @@ class TrainingPlansController < ApplicationController
   def show
     @training_plan = TrainingPlan.find(params[:id])
   end
+
+  def new
+    @training_plan = TrainingPlan.new
+  end
+
+  def create
+    @training_plan = TrainingPlan.new(training_plan_params)
+    @training_plan.user = User.find(1) # hardcoded for now
+
+    if @training_plan.save
+      flash[:success] = "Your Training Plan was created succesfully!"
+      redirect_to training_plans_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def training_plan_params
+      params.require(:training_plan).permit(:name, :description)
+    end
 end
