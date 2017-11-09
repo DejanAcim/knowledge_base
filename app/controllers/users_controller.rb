fd0b9f8702a8ac600537163cce5a1ebf335b8bee
dejanacim <dejanacim@gmail.com>
2017-11-09 20:19:46 +0100

    Implement user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Your account has been created successfully"
+      redirect_to training_plans_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :surname, :email, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
     end
   end
   resources :users, except: [:new]
-  get '/register', to: 'user#new'
+  get '/register', to: 'users#new'
 end
