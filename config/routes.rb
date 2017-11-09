ba39ccc95c2973878b0e82e4f70dce1e7ba83bfa
dejanacim <dejanacim@gmail.com>
2017-11-09 23:32:01 +0100

    Implement login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get '/home', to: 'pages#home'
+  get  '/home', to: 'pages#home'
   resources :training_plans do
     member do
       post 'like'
     end
   end
   resources :users, except: [:new]
-  get '/register', to: 'users#new'
+  get  '/register', to: 'users#new'
+  get  '/login',    to: 'logins#new'
+  post '/login',    to: 'logins#create'
+  get  '/logout',   to: 'logins#destroy'
 end
