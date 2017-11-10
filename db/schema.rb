d48de81753e369655cf7b788802092471e85fea8
dejanacim <dejanacim@gmail.com>
2017-11-10 14:05:17 +0100

    Create skills table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171109135315) do
+ActiveRecord::Schema.define(version: 20171110130358) do
 
   create_table "likes", force: :cascade do |t|
     t.boolean "like"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20171109135315) do
     t.integer "training_plan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "training_plans", force: :cascade do |t|
