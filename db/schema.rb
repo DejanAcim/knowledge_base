0c23f7e24168257b18dc49fbfa27aa5492fb1eef
dejanacim <dejanacim@gmail.com>
2017-11-11 17:52:46 +0100

    Add admin column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171110131932) do
+ActiveRecord::Schema.define(version: 20171111164615) do
 
   create_table "likes", force: :cascade do |t|
     t.boolean "like"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20171110131932) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
