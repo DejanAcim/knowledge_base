22eeb07d100bb9580945c58ee0f6dd0cf7eab5a6
dejanacim <dejanacim@gmail.com>
2017-11-12 20:37:21 +0100

    Add some skills, trainings and training plan seeds

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+skills = %w['Ruby' 'Ruby on Rails' 'JavaScript' 'React' 'CSS' 'HTML' 'Ember' 'Git' 'SQL' 'RSpec' 'JQuery' 'VueJS' 'UX/UI' 'React Native' 'Android' 'Swift']
+trainings = %w['Michael Hartl Ruby on Rails Tutorial'
+               '[Udemy] The Complete Ruby on Rails Develper Course'
+               '[Udemy] Ruby on Rails 5 - BDD, RSpec and Capybara'
+               '[Udemy] Bootstrap 4 From Scratch With 5 Projects'
+               '[Udemy] Modern React with Redux'
+               '[Udemy] Practical CSS Website Development: Crash Course']
+
+training_plans = %w['JavaScript intermediate course'
+                    'Ruby on Rails Testing Plan'
+                    'HTML/CSS Basics course'
+                    'JavaScript basics'
+                    'Ruby on Rails Juniorship plan']
