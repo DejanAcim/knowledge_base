22902810832801251d09f19db3a2eb26db3f2c15
dejanacim <dejanacim@gmail.com>
2017-11-13 22:25:09 +0100

    Add has_secure_password validation to user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe User, type: :model do
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:surname) }
+    it { should have_secure_password }
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_length_of(:password).is_at_least(8).is_at_most(255) }
     it { is_expected.to validate_presence_of(:email) }
