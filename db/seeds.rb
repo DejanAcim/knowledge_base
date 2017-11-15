5a42b676722920f023c36770d14c5da2b7cbb403
dejanacim <dejanacim@gmail.com>
2017-11-15 19:43:30 +0100

    Create basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,35 @@
-skills = %w['Ruby' 'Ruby on Rails' 'JavaScript' 'React' 'CSS' 'HTML' 'Ember' 'Git' 'SQL' 'RSpec' 'JQuery' 'VueJS' 'UX/UI' 'React Native' 'Android' 'Swift']
-trainings = %w['Michael Hartl Ruby on Rails Tutorial'
-               '[Udemy] The Complete Ruby on Rails Develper Course'
-               '[Udemy] Ruby on Rails 5 - BDD, RSpec and Capybara'
-               '[Udemy] Bootstrap 4 From Scratch With 5 Projects'
-               '[Udemy] Modern React with Redux'
-               '[Udemy] Practical CSS Website Development: Crash Course']
+skills = ['Ruby', 'Ruby on Rails', 'JavaScript', 'React', 'CSS', 'HTML', 'Ember', 'Git', 'SQL', 'RSpec', 'JQuery', 'VueJS', 'UX/UI', 'React Native', 'Android', 'Swift']
+trainings = ['Michael Hartl Ruby on Rails Tutorial',
+             '[Udemy] The Complete Ruby on Rails Develper Course',
+             '[Udemy] Ruby on Rails 5 - BDD, RSpec and Capybara',
+             '[Udemy] Bootstrap 4 From Scratch With 5 Projects',
+             '[Udemy] Modern React with Redux',
+             '[Udemy] Practical CSS Website Development: Crash Course']
 
-training_plans = %w['JavaScript intermediate course'
-                    'Ruby on Rails Testing Plan'
-                    'HTML/CSS Basics course'
-                    'JavaScript basics'
-                    'Ruby on Rails Juniorship plan']
+training_plans = ['JavaScript intermediate course',
+                  'Ruby on Rails Testing Plan',
+                  'HTML/CSS Basics course',
+                  'JavaScript basics',
+                  'Ruby on Rails Juniorship plan']
+
+skills.each do |skill|
+  Skill.find_or_create_by(name: skill)
+end
+
+trainings.each do |training|
+  Skill.find_or_create_by(name: skill)
+end
+
+training_plans.each do |plan|
+  tp = TrainingPlan.find_or_create_by( name: plan )
+  tp.update_attributes(description: Faker::Lorem.paragraph, user_id: User.all.ids.sample )
+  rand(6).times do
+    tp.skills << Skill.all.sample
+  end
+  rand(6).times do
+    tp.trainings << Training.all.sample
+  end
+  print '.'
+end
+
+puts 'Seeding fininshed.'
