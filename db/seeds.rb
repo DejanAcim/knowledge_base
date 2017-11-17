db29d2c72dcb0443bb5d3831634189c9ed9845c6
dejanacim <dejanacim@gmail.com>
2017-11-17 14:58:46 +0100

    Add more trainings and Training Plans

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,33 @@
 skills = ['Ruby', 'Ruby on Rails', 'JavaScript', 'React', 'CSS', 'HTML', 'Ember', 'Git', 'SQL', 'RSpec', 'JQuery', 'VueJS', 'UX/UI', 'React Native', 'Android', 'Swift']
 trainings = ['Michael Hartl Ruby on Rails Tutorial',
-             '[Udemy] The Complete Ruby on Rails Develper Course',
-             '[Udemy] Ruby on Rails 5 - BDD, RSpec and Capybara',
-             '[Udemy] Bootstrap 4 From Scratch With 5 Projects',
-             '[Udemy] Modern React with Redux',
-             '[Udemy] Practical CSS Website Development: Crash Course']
+             'The Complete Ruby on Rails Develper Course',
+             'Ruby on Rails 5 - BDD, RSpec and Capybara',
+             'Bootstrap 4 From Scratch With 5 Projects',
+             'Modern React with Redux',
+             'Practical CSS Website Development: Crash Course',
+             'Ultimate web',
+             'Responsive website template from scratch html css',
+             'Web design creating websites from scratch',
+             'Javascript arrays',
+             'Es6 in depth',
+             'MBA business tools that get results free workbooks included',
+             'vue web apps',
+             'earn vue js introduction to simple reactive javascript',
+             'Grid CSS Responsive Website Layout - "Mobile First" Design',
+             'JavaScript Fundamentals For Beginners',
+             'Learn JavaScript By Building A Bookmarker Application',
+             'Simple Weight Converter App With JavaScript & Bootstrap 4',
+             'JSON Crash Course',
+             'Git & GitHub Crash Course For Beginners']
 
 training_plans = ['JavaScript intermediate course',
                   'Ruby on Rails Testing Plan',
                   'HTML/CSS Basics course',
                   'JavaScript basics',
-                  'Ruby on Rails Juniorship plan']
+                  'Ruby on Rails Juniorship plan',
+                  'Company fundamentals',
+                  'Advanced Ruby course',
+                  'Team leadership mastery']
 
 admin = User.create(email: 'admin@example.com',
                     name: 'Admin',
