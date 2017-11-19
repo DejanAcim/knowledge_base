6808e310a64e4c05d18f16555395063a749a8bb2
dejanacim <dejanacim@gmail.com>
2017-11-19 11:49:03 +0100

    Add create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+  before_action :require_user, except: [:show, :index]
+
+  def create
+    @training_plan = TrainingPlan.find(params[:training_plan_id])
+    @comment = @training_plan.comments.new(comment_params)
+    @comment.user = current_user
+    @comment.save!
+
+    redirect_to training_plan_path(@training_plan)
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
